de4c16130b9a6273f8fa40bc3643c891b9ac6c25
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-02-06 20:00:20 +0530

    Create array_duplicate_elements.c

---
+++ array_duplicate_elements.c
@@ -0,0 +1,23 @@
+//Write a program to display the duplicate element of the array.
+
+#include<stdio.h>
+
+int main() {
+    int n, arr[30], i, j;
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array : ");
+    for (i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Duplicate elements in the array : ");
+    for (i=0; i<n; i++){
+        for(j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+                printf("%d ", arr[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
