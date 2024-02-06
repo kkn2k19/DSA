3ce21b146f9126932b7f920afa5e1635098937eb
KKN x CHANDAN <83773820+kkn2k19@users.noreply.github.com>
2024-02-06 20:01:20 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,74 +1,65 @@
 //Write a menu driven program for Stack implementation (PUSH, POP, DISPLAY) using array.
 
 #include<stdio.h>
-#define SIZE 10
+#define MAX 10
 
-int STACK[SIZE];
-int TOP;
-void PUSH(int value);
-void POP();
-void PEEK();
-void DISPLAY();
+int Stack[MAX];
+int top;
+void push(int item);
+void pop();
+void display();
 
-int main() {
-  int value, option;
-  TOP = -1;
-  while (option != 0) {
-    printf("Press 1 for PUSH Operation.\n2 for POP Operation.\n3 for PEEK Operation.\n4 for DISPLAY Operation.\n0 for Exiting the Program. : ");
-    scanf("%d", &option);
-    switch (option) {
-      case 1:
-          printf("Enter value to PUSH into STACK. : \n");
-          scanf("%d", &value);
-          PUSH(value);
-          break;
-      case 2:
-          POP();
-          break;
-      case 3:
-          PEEK();
-          break;
-      case 4:
-          DISPLAY();
-          break;
-      case 0:
-          printf("EXITING.\n");
+int main(){
+    int item, option;
+    top=-1;
+    while(option != 0){
+        printf("Press 1 for PUSH,\n2 for POP,\n3 for DISPLAY,\n0 for EXIT. : ");
+        scanf("%d", &option);
+        switch(option){
+        case 1:
+            printf("Enter number  for PUSH : ");
+            scanf("%d", &item);
+            push(item);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 0:
+            printf("EXITING.....\n");
+            break;
+        default:
+            printf("ENTER INPUT BETWEEN 0 to 3\n");
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
-void PUSH(int value) {
-  if (TOP == SIZE-1) {
-    printf("OVERFLOW.\n");
-  } else {
-    TOP++;
-    STACK[TOP] = value;
-  }
+void push(int item){
+    if (top==MAX-1){
+        printf("OVERFLOW.\n");
+    }
+    else{
+        top=top+1;
+        Stack[top]=item;
+    }
 }
 
-void POP() {
-  int value;
-  if (TOP == -1) {
-    printf("UNDERFLOW.\n");
-  } else {
-    value = STACK[TOP];
-    printf("POPED VALUE : %d\n", value);
-    TOP--;
-  }
+void pop(){
+    if (top==-1){
+        printf("UNDERFLOW.\n");
+    }
+    else{
+       printf("POPED VALUE : %d\n", Stack[top]);
+       top--;
+    }
 }
 
-void PEEK() {
-  if (TOP == -1) {
-    printf("UNDERFLOW.\n");
-  } else {
-    printf("PEEK VALUE OF THE STACK : %d\n",  STACK[TOP]);
-  }
-}
-
-void DISPLAY() {
-  int i;
-  for (i = TOP; i>=0; i--) {
-    printf("%d\n", STACK[i]);
-  }
+void display(){
+    int i;
+    for (i=top; i>=0; i--){
+        printf("%d \n", Stack[i]);
+    }
 }
